556627083be13c30b00be71c8736e0d272f491a1
Dyrek Wells <blupandaraid60@gmail.com>
2022-08-17 15:35:38 -1000

    update code

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Login extends Component {
     constructor(props) {
@@ -6,7 +8,8 @@ export default class Login extends Component {
 
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            errorText: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -15,12 +18,39 @@ export default class Login extends Component {
 
     handleChange(event) {
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
+            errorText: ""
         });
     }
 
     handleSubmit(event) {
-        console.log("Handle submit", this.state.email, this.state.password);
+        axios.post("https://api.devcamp.space/sessions",
+        {
+            client: {
+                email: this.state.email,
+                password: this.state.password
+            }
+        },
+        { withCredentials: true }
+        )
+        .then(response => {
+            if (response.data.status === "created") {
+                this.props.handleSuccessfulAuth();
+            } else {
+                this.setState({
+                    errorText: "Wrong email or password"
+                });
+                this.props.handleUnSuccessfulAuth();
+            }
+        })
+        .catch(error => {
+            this.setState({
+                errorText: "An error occured"
+            });
+            this.props.handleUnSuccessfulAuth();
+        });
+        
+
         event.preventDefault();
     }
 
@@ -29,26 +59,32 @@ export default class Login extends Component {
             <div>
             <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
-            <form onSubmit={this.handleSubmit}>
-                <input 
-                    type="email"
-                    name="email"
-                    placeholder="Your email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    />
+            <div>{this.state.errorText}</div>
 
-                <input 
-                type="password"
-                name="password"
-                placeholder="Your password"
-                value={this.state.password}
-                onChange={this.handleChange}
-                />
-
-                <div>
-                    <button type="submit">Login</button>
+            <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+                <div className="form-group">
+                    <FontAwesomeIcon icon="envelope" />
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Your email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        />
                 </div>
+
+                <div className="form-group">
+                    <FontAwesomeIcon icon="lock" />
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Your password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        />
+                </div>
+
+                    <button className="btn" type="submit">Login</button>
             </form>
             </div>
         );
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,12 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
-export default class NavigationComponent extends Component {
-    constructor() {
-        super();
-    }
+const NavigationComponent = props => {
+    const dynamicLink = (route, linkText) => {
+        return (
+            <div className="nav-link-wrapper">
+                <NavLink to={route} activeClassName="nav-link-active">
+                    {linkText}
+                </NavLink>
+            </div>
+        );
+    }; 
 
-    render() {
+    const handleSignOut = () => {
+        axios.delete("https://api.devcamp.space/logout", { withCredentials: true })
+        .then(response => {
+            if (response.status === 200) {
+                props.history.push("/");
+                props.handleSuccessfulLogout();
+            }
+            return response.data;
+        })
+        .catch(error => {
+            console.log("Error signing out", error);
+        });
+    };
+
         return (
             <div className="nav-wrapper">
                 <div className="left-side">
@@ -33,12 +55,22 @@ export default class NavigationComponent extends Component {
                     Blog
                 </NavLink>
             </div>
+
+           {props.loggedInStatus === "LOGGED_IN" ? (
+            dynamicLink("/portfolio-manager", "Portfolio Manager")
+            ) : null}  
         </div>
 
                 <div className="right-side">
                     Dyrek Wells
+                    {props.loggedInStatus === "LOGGED_IN" ? (
+                        <a onClick={handleSignOut}>
+                            <FontAwesomeIcon icon="sign-out-alt" />
+                        </a>
+                    ) : null}
                 </div>
             </div>
         );
     }
-}
+
+export default withRouter(NavigationComponent);
